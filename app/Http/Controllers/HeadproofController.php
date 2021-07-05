77a139fcb46fa496ecf40050696dbd50511aad18
Paula Pereyra <paulapereyrallorens@gmail.com>
2021-07-05 18:13:15 -0300

    Obtener nombres articulos en el comprobante.

--- app/Http/Controllers/HeadproofController.php
+++ app/Http/Controllers/HeadproofController.php
@@ -90,9 +90,12 @@ class HeadproofController extends Controller
 
     public function show($id)
     {
-        $detailsReceipt = DB::table('lineproofs')
-                ->where('headproof_id', '=', $id)
-                ->get();
+        $detailsReceipt =  DB::table('lineproofs')
+        ->join('articles','lineproofs.article_id','=','articles.id')
+        ->select('lineproofs.id','articles.nameArticle','lineproofs.headproof_id')
+        ->where('headproof_id', '=', $id)
+        ->get();
+
         return $detailsReceipt->toJson(JSON_PRETTY_PRINT);
     }
     
