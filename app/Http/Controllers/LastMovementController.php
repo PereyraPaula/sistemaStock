5a1096f3805336611251aedff0a8e637c832c94c
Paula Pereyra <paulapereyrallorens@gmail.com>
2022-01-16 11:46:02 -0300

    Paginado seccion - Ultimos movimientos -

--- app/Http/Controllers/LastMovementController.php
+++ app/Http/Controllers/LastMovementController.php
@@ -1,8 +1,9 @@
 <?php
-
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+
+use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\DB;
 use Carbon\Carbon;
 
@@ -13,13 +14,26 @@ class LastMovementController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
-        $lastMovements = DB::table('headproofs')
+        $maxPage = 5;
+
+        $movements = DB::table('headproofs')
         ->join('lineproofs','lineproofs.headproof_id','=','headproofs.id')
         ->select('headproofs.id','headproofs.type_movement','headproofs.date_movement','lineproofs.quantity_movement')->orderBy('date_movement','desc')
-        ->get();
+        ->paginate(5);
 
-        return $lastMovements->toJson(JSON_PRETTY_PRINT);
+        return [
+            "pagination"=>[
+                'total' => $movements->total(),
+                'current_page' => $movements->currentPage(),
+                'per_page' => $movements->perPage(),
+                'last_page' => $movements->lastPage(),
+                'from' => $movements->firstItem(),
+                'to' => $movements->lastPage(),
+            ],
+            "movements" => $movements
+        ];
+
     }
 }
